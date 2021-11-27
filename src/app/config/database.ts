d86b4bfad1803eba51562e30ea768deb04164629
Edgar Andrés Angrino Lafaux <mix7reload@gmail.com>
2021-11-27 02:02:25 -0500

    Update database.ts
    cuando intente conectar a la base de datos y los datos no sean correctos mostrar el error

--- src/app/config/database.ts
+++ src/app/config/database.ts
@@ -9,6 +9,19 @@ const pool = new Pool({
   port: Number(process.env.PG_PORT),
 });
 
-console.log(`⚡️ Database is started⚡️`);
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+
+  client.query("SELECT NOW()", (err, result) => {
+    release();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+
+    console.log(`⚡️ The database was started at ${result.rows[0].now}⚡️`);
+  });
+});
 
 export { pool };
