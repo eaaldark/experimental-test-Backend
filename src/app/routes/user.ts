7f813c1b3b6fecb6d67bd8bf457a9354343cffc3
Edgar Andrés Angrino Lafaux <mix7reload@gmail.com>
2021-11-30 07:35:48 -0500

    Update user.ts
    Se retiran las funciones para crear el controlador de usuario, este archivo solo manejara las rutas

--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -1,64 +1,9 @@
-import { Request, Response, Router } from "express";
-import { QueryResult } from "pg";
-import { pool } from "../config/database";
-import sign from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import cors from "cors";
+import { Router } from "express";
+import { login, register } from "../controllers/controller.user";
 
 const userRouter = Router();
 
-const SALT = "64";
-const EXPIRES = "1h";
-
-userRouter.post(
-  "/login",
-  async (req: Request, res: Response): Promise<Response> => {
-    let { email, password } = req.body;
-    email = email.toLowerCase();
-
-    console.log(email, password);
-
-    try {
-      const response: QueryResult = await pool.query("select now()");
-      return res.status(200).json(response.rows);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ Tripalosky: "Xd" });
-    }
-  }
-);
-
-userRouter.post(
-  "/register",
-  async (req: Request, res: Response): Promise<Response> => {
-    const {
-      email,
-      password,
-      firstname,
-      middlename,
-      lastname,
-      birthday,
-      phonenumber,
-    } = req.body;
-
-    console.log(
-      email,
-      password,
-      firstname,
-      middlename,
-      lastname,
-      birthday,
-      phonenumber
-    );
-
-    try {
-      const response: QueryResult = await pool.query("select now()");
-      return res.status(200).json(response.rows);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ Tripalosky: "Xd" });
-    }
-  }
-);
+userRouter.post("/login", login);
+userRouter.post("/register", register);
 
 export { userRouter };
