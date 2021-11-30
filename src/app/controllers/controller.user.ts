640b82e761a0248a9678df49df324a1b0ad440b8
Edgar Andrés Angrino Lafaux <mix7reload@gmail.com>
2021-11-30 07:35:12 -0500

    Create controller.user.ts
    Se crea el controlador de usuarios para mejor manejo del codigo

---
+++ src/app/controllers/controller.user.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { postgreSQLConnect, QueryResult } from "../config/database";
+import sign from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import cors from "cors";
+
+const login = async (req: Request, res: Response): Promise<Response> => {
+  let { email, password } = req.body;
+  email = email.toLowerCase();
+
+  console.log(email, password);
+
+  try {
+    const response: QueryResult = await postgreSQLConnect.query("select now()");
+    return res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ Tripalosky: "Xd" });
+  }
+};
+
+const register = async (req: Request, res: Response): Promise<Response> => {
+  const {
+    email,
+    password,
+    firstname,
+    middlename,
+    lastname,
+    birthday,
+    phonenumber,
+  } = req.body;
+
+  console.log(
+    email,
+    password,
+    firstname,
+    middlename,
+    lastname,
+    birthday,
+    phonenumber
+  );
+
+  try {
+    const response: QueryResult = await postgreSQLConnect.query("select now()");
+    return res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ Tripalosky: "Xd" });
+  }
+};
+
+export { login, register };
