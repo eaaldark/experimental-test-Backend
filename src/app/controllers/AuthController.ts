4d6104961ac92aa2d970e1a65ff38283717c531f
eaaldark <mix7reload@gmail.com>
2022-01-07 17:36:21 -0500

    Update AuthController.ts
    Primer configuracion con jwt en practica

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -2,17 +2,22 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { postgreSQLConnect, QueryResult } from "../config/database";
-import cors from "cors";
+import fs from "fs";
 
 const login = async (req: Request, res: Response): Promise<Response> => {
   let { email, password } = req.body;
-  email = email.toLowerCase();
-
-  console.log(email, password);
 
   try {
-    const response: QueryResult = await postgreSQLConnect.query("select now()");
-    return res.status(200).json(response.rows);
+    // const response: QueryResult = await postgreSQLConnect.query("select now()");
+    // return res.status(200).json(response.rows);
+    const user = {
+      email: email.toLowerCase(),
+      password: password,
+    };
+
+    var privateKey = fs.readFileSync("private.key");
+    let token = jwt.sign( user , privateKey, { algorithm: "HS256", expiresIn: "1h" });
+    return res.status(200).json(token);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ Tripalosky: "Xd" });
